91da31a6159cc1a3180864362cdaf9751bf83ef8
Kishan S Patel <bishan.batel@protonmail.com>
2024-11-10 01:30:37 -0800

    Setup

--- flake.nix
+++ flake.nix
@@ -10,17 +10,38 @@
       let
         pkgs = import nixpkgs { inherit system; };
         naersk-lib = pkgs.callPackage naersk { };
+        libPath = with pkgs; 
+          lib.makeLibraryPath [ 
+            libffi
+            wayland-protocols
+            wayland
+            libGL
+            xorg.libxcb
+
+            xorg.libX11
+            xorg.libX11
+            xorg.libXrandr
+            xorg.libXinerama
+            xorg.libXcursor
+            xorg.libXi
+
+            libxkbcommon
+
+            libglvnd
+          ]; 
       in
       {
         defaultPackage = naersk-lib.buildPackage ./.;
         devShell = with pkgs; mkShell {
           nativeBuildInputs = [
             wayland-protocols
+            libxkbcommon
             wayland
           ];
 
-          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy ];
+          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy wasm-pack];
           RUST_SRC_PATH = rustPlatform.rustLibSrc;
+          LD_LIBRARY_PATH = libPath;
         };
       }
     );
